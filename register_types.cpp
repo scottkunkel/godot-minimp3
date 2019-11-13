075929ff810cde3f2f9e8d6e8875c9fc8c931508
TSK <thomas.scott.kunkel@gmail.com>
2019-11-13 10:17:18 -0500

    reorganization

--- register_types.cpp
+++ register_types.cpp
@@ -1,5 +1,5 @@
 /*************************************************************************/
-/*  register_types.h                                                     */
+/*  register_types.cpp                                                   */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
@@ -28,5 +28,26 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-void register_minimp3_types();
-void unregister_minimp3_types();
+#include "register_types.h"
+
+#include "audio_stream_mp3.h"
+
+#ifdef TOOLS_ENABLED
+#include "core/engine.h"
+#include "resource_importer_mp3.h"
+#endif
+
+void register_mp3_types() {
+
+#ifdef TOOLS_ENABLED
+	if (Engine::get_singleton()->is_editor_hint()) {
+		Ref<ResourceImporterMP3> mp3_import;
+		mp3_import.instance();
+		ResourceFormatImporter::get_singleton()->add_importer(mp3_import);
+	}
+#endif
+	ClassDB::register_class<AudioStreamMP3>();
+}
+
+void unregister_mp3_types() {
+}
